b9e5152cf3b79b9df15726d2716ffa1670a159ef
Tomotaka Kato <tomo@Tomotakas-MacBook-Pro.local>
2022-07-24 22:03:26 +0900

    opencvのライブラリを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+    entry: './src/index.ts',
 
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'production',
+    devtool: 'inline-source-map',
 
-  module: {
-    rules: [{
-      test: /\.ts$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }]
-  },
-  resolve: {
-    extensions: ['.ts','.js']
-  }
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.ts','.js']
+    },
 }
