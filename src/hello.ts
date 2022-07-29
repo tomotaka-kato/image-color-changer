c4c55ee3c543198158f184baf7b68e1cfe70559b
Tomotaka Kato <tomo@Tomotakas-MacBook-Pro.local>
2022-07-29 20:43:25 +0900

    家の画像ファイルを追加

--- src/hello.ts
+++ src/hello.ts
@@ -2,3 +2,25 @@ export const hello = (name: string): string => {
     return `Hello, ${name}!!`
 }
 
+
+class Sample {
+     log_arg = (arg: string) => {
+        console.log(arg)
+        console.log(arg)
+        console.log(arg)
+        console.log(arg)
+
+
+        console.log(arg)
+        console.log(arg)
+    }
+
+    log_arg_2(arg: string) {
+        console.log(arg)
+        console.log(arg)
+        console.log(arg)
+        console.log(arg)
+        console.log(arg)
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+import cv from "@techstark/opencv-js";
 import { hello } from './hello'
 console.log(hello("World"));
